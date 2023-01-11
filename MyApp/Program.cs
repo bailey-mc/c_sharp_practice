382f88b1f0a1c15d2c4e46ff24302e414dc64984
Bailey McDonald <mcdonald.bailey@gmail.com>
2023-01-11 16:18:02 -0600

    can work once w/ workToday() and make money from it w/ makeMoney()

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -2,23 +2,13 @@
 
 class Program {
 
-  class Landscaper {
-    public string name;
-    public int bank;
-    // public toolbox[] tools;
-
-    //constructor
-    public Landscaper( string name, int bank){
-      this.name = name;
-      this.bank = bank;
-    }
-  }
-
+  //Tool class
   class Tool {
     public string name;
     public int cost;
     public int returns;
 
+    //constructor
     public Tool( string name, int cost, int returns){
       this.name = name;
       this.cost = cost;
@@ -26,23 +16,67 @@ class Program {
     }
 
   }
-  
-  public static void Main (string[] args) {
 
+  //toolbox is created
+  static Tool[] toolbox = {new Tool("teeth", 0, 1), new Tool("scissors", 5, 5), new Tool("old lawnmower", 25, 50), new Tool("nice new lawnmower", 250, 100), new Tool("starving students", 500, 250)};
+
+  //Landscaper class
+    class Landscaper {
+    public string name;
+    public int bank;
+    // public static Tool[] tools = toolbox;
+    public int numOfTools;
+
+    //constructor
+    public Landscaper( string name, int bank, int numOfTools){
+      this.name = name;
+      this.bank = bank;
+      this.numOfTools = numOfTools;
+     
+    }
+
+    public void makeMoney() {
+      bank += toolbox[numOfTools - 1].returns;
+      Console.WriteLine("You made $" + toolbox[numOfTools - 1].returns + " today. You now have $" + bank + " in the bank");
+    }
+
+    public void workToday() {
+    //set variable for response
+    string work;
+    Console.WriteLine("Would you like to work today? press y for yes or n for no");
+    work = Console.ReadLine();
+    if (work == "y") {
+      makeMoney();
+    } else {
+      Console.WriteLine("you chose not to work today");
+    }
+  }
+
+  }
+
+
+
+  //HERE'S WHERE THE ACTION HAPPENS
+  public static void Main (string[] args) {
+    //set variable for input
     string input;
     Console.WriteLine("What's your name?");
-    //erceive input
+    //receive input
     input = Console.ReadLine();
     //print message
     Console.WriteLine("Hello " + input);
+
+    //landscaper 'instantiated' as player. no money
+    Landscaper landscaper = new Landscaper(input, 0, 1);
+
+    // Console.WriteLine(landscaper.name + " name, money : " + landscaper.bank + "tools? " + toolbox[landscaper.numOfTools - 1].name);
+
+    landscaper.workToday();
     
-    Tool[] toolbox = new Tool[2];
-    Tool teeth = new Tool("teeth", 0, 1);
-    Tool scissors = new Tool("scissors", 5, 5);
 
-    Landscaper landscaper = new Landscaper(input, 0);
-
-    Console.WriteLine(landscaper.name + " name, money : " + landscaper.bank);
+    // foreach (Tool tool in toolbox){
+    //   Console.WriteLine("name: " + tool.name + ", cost: " + tool.cost + ", return: " + tool.returns);
+    // }
   }
 }
 
