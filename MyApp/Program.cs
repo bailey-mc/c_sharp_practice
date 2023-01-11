8fbd1ad7f81d88d644ab5c70a5ae98c25736619f
Bailey McDonald <mcdonald.bailey@gmail.com>
2023-01-11 17:22:06 -0600

    landscaper game fully functional

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -18,10 +18,10 @@ class Program {
   }
 
   //toolbox is created
-  static Tool[] toolbox = {new Tool("teeth", 0, 1), new Tool("scissors", 5, 5), new Tool("old lawnmower", 25, 50), new Tool("nice new lawnmower", 250, 100), new Tool("starving students", 500, 250)};
+  static Tool[] toolbox = {new Tool("set of teeth", 0, 1), new Tool("pair of rusty scissors", 5, 5), new Tool("old lawnmower", 25, 50), new Tool("nice new lawnmower", 250, 100), new Tool("starving students", 500, 250), new Tool("placeholder", 2000, 0)};
 
   //Landscaper class
-    class Landscaper {
+  class Landscaper {
     public string name;
     public int bank;
     // public static Tool[] tools = toolbox;
@@ -35,22 +35,77 @@ class Program {
      
     }
 
-    public void makeMoney() {
-      bank += toolbox[numOfTools - 1].returns;
-      Console.WriteLine("You made $" + toolbox[numOfTools - 1].returns + " today. You now have $" + bank + " in the bank");
+    void goShopping() {
+      //set variable to receive input//really just so they have to press enter for game to progress... it's fun to have buttons to push
+      string input;
+      Console.WriteLine("You have $" + bank + " in the bank. That's enough to buy a " + toolbox[numOfTools].name + " for $" + toolbox[numOfTools].cost + ". With a " + toolbox[numOfTools].name + " you will be able to earn $" + toolbox[numOfTools].returns + " a day.");
+      //subtract cost of next tool from bank acct
+      bank -= toolbox[numOfTools].cost;
+      //add to numOfTools ---> all toolbox[numOfTools] info moves up to next index
+      numOfTools ++;
+      Console.WriteLine("***********************************");
+      Console.WriteLine("You have successfully purchased a " + toolbox[numOfTools - 1].name + "! You now have $" + bank + " in the bank.");
+      input = Console.ReadLine();
+      
+      //win condition
+      if ((numOfTools == 5) && (bank >= 1000)) {
+        Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+        Console.WriteLine("You have a " + toolbox[numOfTools - 1].name + " and $" + bank + " in the bank! You win!");
+        Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      }else {
+        Console.WriteLine("Ain't no rest for the wicked, huh? back to work for you!");
+        Console.WriteLine("*****************************************");
+        workToday();
+      }
     }
 
-    public void workToday() {
-    //set variable for response
-    string work;
-    Console.WriteLine("Would you like to work today? press y for yes or n for no");
-    work = Console.ReadLine();
-    if (work == "y") {
-      makeMoney();
-    } else {
-      Console.WriteLine("you chose not to work today");
+    void wannaGoShopping() {
+      //set variable to receive input
+      string input;
+      Console.WriteLine("you have enough money to buy a " + toolbox[numOfTools].name);
+      input = Console.ReadLine();
+      Console.WriteLine("Would you like to go shopping? please enter y for yes or n for no");
+      input = Console.ReadLine();
+      if (input == "y") {
+        //if you choose to go shopping, calls function to buy tool
+        goShopping();
+      } else {
+        Console.WriteLine("back to work it is");
+        Console.WriteLine("*********************************");
+        workToday();
+      }
     }
-  }
+
+    void makeMoney() {  
+      //increment bank acct by return of tool landscaper currently has
+      bank += toolbox[numOfTools - 1].returns;
+      Console.WriteLine("You made $" + toolbox[numOfTools - 1].returns + " today. You now have $" + bank + " in the bank");
+      //win condition
+      if ((numOfTools == 5) && (bank >= 1000)) {
+        Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+        Console.WriteLine("You have a " + toolbox[numOfTools - 1].name + " and $" + bank + " in the bank! You win!");
+        Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      //if you don't have enough money in your bank to buy the next tool, you are asked if you want to work again
+      }else if (bank < toolbox[numOfTools].cost) {
+        workToday();
+      //if you do have enough money to buy next tool, you are asked if you want to go shopping
+      } else {
+        wannaGoShopping();
+      }
+    }
+
+
+    public void workToday() {
+      //set variable for response
+      string work;
+      Console.WriteLine("Would you like to work today? press y for yes or n for no");
+      work = Console.ReadLine();
+      if (work == "y") {
+        makeMoney();
+      } else {
+        Console.WriteLine("you chose not to work today");
+      }
+    }
 
   }
 
@@ -69,92 +124,9 @@ class Program {
     //landscaper 'instantiated' as player. no money
     Landscaper landscaper = new Landscaper(input, 0, 1);
 
-    // Console.WriteLine(landscaper.name + " name, money : " + landscaper.bank + "tools? " + toolbox[landscaper.numOfTools - 1].name);
-
+    //go to work, get into the meat of the game
     landscaper.workToday();
     
-
-    // foreach (Tool tool in toolbox){
-    //   Console.WriteLine("name: " + tool.name + ", cost: " + tool.cost + ", return: " + tool.returns);
-    // }
   }
 }
 
-//CODE GRAVEYARD
-// using System;
-
-// class Program {
-  
-//   class Creature{
-    
-//     protected string name;
-//     protected int age;
-//     protected int legs;
-//     protected bool goodBoy;
-
-//     //constructor function --runs this function when the object is created
-//     public Creature(string nm, int ag, int leg, bool good){
-//       name = nm;
-//       age = ag;
-//       legs = leg;
-//       goodBoy = good;
-//     }
-
-//     // this second version of the constructor is so that if someone passes in nothing, nothing is created
-//     public Creature() {
-      
-//     }
-//     //destructor function --runs this function when the object is destroyed
-//     ~Creature(){
-//       Console.WriteLine(this.name + " has died at the age of " + this.age);
-//     }
-
-//     public void aging (){
-//         age++;
-//         Console.WriteLine(this.name + " is now of age " + this.age);
-//     }
-//   }
-//     //new Dog class that will inherit properties of Creature class
-//     class Dog : Creature{
-//         //Dog gets its own constructor
-//         //Creature constructor is run first, then dog constructor is run after
-//         //this is where empty constructor on line26 comes in... so program can call creature constructor w/ no parameters passed in w/o breaking. then it can call dog constructor under/into that
-//         public Dog (string nm, int ag){
-//             name = nm;
-//             age = ag;
-//             legs = 4;
-//             goodBoy = true;
-//         }
-
-//         //public functions so we can call on them outside of the class definition
-//         public void bark(){
-//             Console.WriteLine("Bark");
-//         }
-
-//         public void info(){
-//             Console.WriteLine("name: " + this.name + ", age: " + this.age + ", legs: " + this.legs + ", good boy? " + this.goodBoy);
-//         }
-//     }
-    
-//     static int addFive (int x) {
-//         Console.WriteLine("testingggg");
-//         return x + 5;
-//     }
-  
-//   public static void Main (string[] args) {
-
-//     Creature sukiCat = new Creature("Suki", 3, 4, true);
-//     sukiCat.aging();
-//     sukiCat.aging();
-
-//     Dog tucker = new Dog("Tucker", 8);
-//     tucker.aging();
-//     tucker.bark();
-//     tucker.info();
-//     tucker.bark();
-
-//     Console.WriteLine(addFive(6));
-
-    
-//   }
-// }
